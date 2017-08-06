c6af494a401f3b2bf4ddeb0b3b197d27a5a61887
zsmb13 <braunmarci@gmail.com>
2017-08-06 16:55:41 +0200

    Removed Android module

--- koin-android/app/src/main/kotlin/koin/sampleapp/DailyForecastModel.kt
+++
@@ -1,7 +0,0 @@
-package fr.ekito.myweatherapp
-
-data class DailyForecastModel(val forecastString: String, val icon: String, val temperatureLow: String, val temperatureHigh: String) {
-
-    val temperatureString = "$temperatureLow°C - $temperatureHigh°C"
-
-}
--- koin-android/koin-android/src/main/kotlin/org/koin/android/error/KoinApplicationException.kt
+++
@@ -1,6 +0,0 @@
-package org.koin.android.error
-
-/**
- * Created by arnaud on 22/06/2017.
- */
-class KoinApplicationException(msg: String) : Exception(msg)
